f3db64b0063f5ab5a18370ead87f98d4bb2d70eb
pixels___ <79591516+bit-pixels@users.noreply.github.com>
2023-04-03 21:20:21 +0100

    Committed lessons 6-10

--- Examples/A [9] If statements.py
+++ Examples/A [9] If statements.py
@@ -1,7 +1,7 @@
 # if statement = a block of code that will execute if it's condition is true
 # elif statement = a block of code that will execute if the previous if statement is false and it's condition is true
 # else statement = a block of code that will execute if all the previous if and elif statements are false
-
+ 
 age = int(input("How old are you?: "))
 
 if age == 100:
